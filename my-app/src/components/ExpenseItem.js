7598f8980a05993c3ebcab5002446c47cdf5df5f
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.localdomain>
2023-02-06 09:58:21 -0600

    expence Item CSS is done

---
+++ my-app/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (<div>
+        <div>Feb 6 2023</div>
+        <div>
+            <h2>Car Isurance</h2>
+            <div>$999</div>
+        </div>
+    </div>
+    )
+};
+
+
+export default ExpenseItem;

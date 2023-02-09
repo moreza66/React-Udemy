25b860a1d7cd42d372d09c972458fc1f3bb93cf7
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.localdomain>
2023-02-09 11:13:41 -0600

    start adding bar height

--- my-app/src/components/Chart/ChartBar.js
+++ my-app/src/components/Chart/ChartBar.js
@@ -3,19 +3,14 @@ import React from "react";
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
-  return <div className="Chart-bar">
-    <div className="chart-bar__inner">
+  return (
+    <div className="Chart-bar">
+      <div className="chart-bar__inner">
         <div className="chart-bar__fill"></div>
-        <div className="chart-bar__label"></div>
+        <div className="chart-bar__label">{props.label}</div>
+      </div>
     </div>
-  </div>    
-   {props.dataPoints.map((dataPoints) => (
-        <ChartBar
-          key={dataPoints.label}
-          value={dataPoints.value}
-          maxValue={null}
-          label={dataPoints.label}
-        />
+  );
 };
 
 export default ChartBar;
